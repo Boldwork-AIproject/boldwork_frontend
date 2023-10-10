4609624583329e64d85109fc01fb6d031aebd58c
ganyunhee <eunicenellgan@gmail.com>
2023-10-11 08:11:57 +0900

    Start developing main page

--- src/Main.js
+++ src/Main.js
@@ -8,8 +8,12 @@ import { Nav, Navbar, Container, Col, Row } from 'react-bootstrap';
 const Main = () => {
     return (
         <>
-            <h1 class="title">AI로 보다 똑똑한 상담을!</h1>
-            <p class="subtitle">지금 바로 상담 내용을 자동으로 분석하고 정리해주는 똑똑한 AI와 함께, 편리한 콜상담을 경험해보세요.</p>
+            <Container id="main">
+                <img class="logo" src="/bold/logo.png"></img>
+                <h1 id="title">AI로 보다 똑똑한 상담을!</h1>
+                <p id="subtitle">지금 바로 상담 내용을 자동으로 분석하고 정리해주는<br/>똑똑한 AI와 함께, 편리한 콜상담을 경험해보세요.</p>
+            </Container>
+            <img id="main_bg"src="/bold/main_bg.png"></img>
         </>
     );
 }
