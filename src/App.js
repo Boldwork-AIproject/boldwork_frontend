b6caf4f7a2941c50300d6f76d730d02c7ff17a2c
ganyunhee <eunicenellgan@gmail.com>
2023-10-11 02:43:24 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Nav, Navbar, Container, Col, Row } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Navbar>
+        <Nav>
+          <Navbar.Brand href="#main"><img src="/bold/logo.png" alt="Logo" width="100px" height="31px"></img></Navbar.Brand>
+          <Nav.Link href="">AI 상담 분석</Nav.Link>
+          <Nav.Link href="">지난 상담 확인</Nav.Link>
+          <Nav.Link href="">고객정보 확인</Nav.Link>
+          <Nav.Link href="">회원가입</Nav.Link>
+          <Nav.Link href="">로그인</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Container id="login">
+        <h1>환영합니다!</h1>
+        <h1><span>로그인</span>을 부탁드려요!</h1>
+      </Container>
+    </>
   );
 }
 
