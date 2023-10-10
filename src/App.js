bc25412978cd36e1040c22f62c2868e0bd3e7129
ganyunhee <eunicenellgan@gmail.com>
2023-10-11 05:04:08 +0900

    Edit navigation router

--- src/App.js
+++ src/App.js
@@ -15,13 +15,12 @@ function App() {
         <p class="subtitle">지금 바로 상담 내용을 자동으로 분석하고 정리해주는
 똑똑한 AI와 함께, 편리한 콜상담을 경험해보세요.</p>
       </Container>
-
+                  
       <Routes>
-        <Route path="/login" component={Login} />
+            <Route path="/login" component={Login} />
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
     </>
-    
   );
 }
 
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import Login from './Login.js';
+import App from './App.js';
+import { BrowserRouter, Router, Route, Routes, Link } from 'react-router-dom';
+import { Nav, Navbar, Container, Col, Row } from 'react-bootstrap';
 
 const Navigation = () => {
     return (
-        <Navbar>
-            <Nav>
-                <Navbar.Brand as={Link} to="/"><img src="/bold/logo.png" alt="Logo" width="100px" height="31px"></img></Navbar.Brand>
-                <Nav.Link>AI 상담 분석</Nav.Link>
-                <Nav.Link>지난 상담 확인</Nav.Link>
-                <Nav.Link>고객정보 확인</Nav.Link>
-                <Nav.Link>회원가입</Nav.Link>
-                <Nav.Link as={Link} to="/login">로그인</Nav.Link>
-            </Nav>
-        </Navbar>
+            <Navbar>
+                <Nav>
+                    <Navbar.Brand as={Link} to="/"><img src="/bold/logo.png" alt="Logo" width="100px" height="31px"></img></Navbar.Brand>
+                    <Nav.Link>AI 상담 분석</Nav.Link>
+                    <Nav.Link>지난 상담 확인</Nav.Link>
+                    <Nav.Link>고객정보 확인</Nav.Link>
+                    <Nav.Link>회원가입</Nav.Link>
+                    <Nav.Link as={Link} to="/login">로그인</Nav.Link>
+                </Nav>
+            </Navbar>
     );
 }
 
