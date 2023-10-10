8bcd21aae7c566392c7065cec16db2b595ab3dc9
ganyunhee <eunicenellgan@gmail.com>
2023-10-11 06:36:20 +0900

    Finish navigation menu layout
     Please enter the commit message for your changes. Lines starting
    
     Date:      Wed Oct 11 06:36:20 2023 +0900
    
     On branch main
     Changes to be committed:
    	modified:   src/App.css
    	modified:   src/Navigation.js
    
     Changes not staged for commit:
    	modified:   src/App.js
    	modified:   src/Login.js
    	modified:   src/index.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './Login.js';
-import App from './App.js';
+import Main from './Main.js'
 import { BrowserRouter, Router, Route, Routes, Link } from 'react-router-dom';
 import { Nav, Navbar, Container, Col, Row } from 'react-bootstrap';
 
@@ -9,11 +9,12 @@ const Navigation = () => {
             <Navbar>
                 <Nav>
                     <Navbar.Brand as={Link} to="/"><img src="/bold/logo.png" alt="Logo" width="100px" height="31px"></img></Navbar.Brand>
-                    <Nav.Link>AI 상담 분석</Nav.Link>
-                    <Nav.Link>지난 상담 확인</Nav.Link>
-                    <Nav.Link>고객정보 확인</Nav.Link>
-                    <Nav.Link>회원가입</Nav.Link>
-                    <Nav.Link as={Link} to="/login">로그인</Nav.Link>
+                    <Nav.Link as={Link} to="upload">AI 상담 분석</Nav.Link>
+                    <Nav.Link as={Link} to="check">지난 상담 확인</Nav.Link>
+                    <Nav.Link as={Link} to="info">고객정보 확인</Nav.Link>
+                    <Nav.Link as={Link} to="signup">회원가입</Nav.Link>
+                    <Nav.Link>|</Nav.Link>
+                    <Nav.Link as={Link} to="login">로그인</Nav.Link>
                 </Nav>
             </Navbar>
     );
