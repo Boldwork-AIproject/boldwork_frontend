98e5f79165f904e3295dff83cca999c5135d4066
ganyunhee <eunicenellgan@gmail.com>
2023-10-11 03:17:50 +0900

    Add initial colors and main page background

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
     <>
      <Navbar>
         <Nav>
-          <Navbar.Brand href="#main"><img src="/bold/logo.png" alt="Logo" width="100px" height="31px"></img></Navbar.Brand>
-          <Nav.Link href="">AI 상담 분석</Nav.Link>
-          <Nav.Link href="">지난 상담 확인</Nav.Link>
-          <Nav.Link href="">고객정보 확인</Nav.Link>
-          <Nav.Link href="">회원가입</Nav.Link>
-          <Nav.Link href="">로그인</Nav.Link>
+          <Navbar.Brand href="/"><img src="/bold/logo.png" alt="Logo" width="100px" height="31px"></img></Navbar.Brand>
+          <Nav.Link href="upload">AI 상담 분석</Nav.Link>
+          <Nav.Link href="check">지난 상담 확인</Nav.Link>
+          <Nav.Link href="info">고객정보 확인</Nav.Link>
+          <Nav.Link href="signup">회원가입</Nav.Link>
+          <Nav.Link href="login">로그인</Nav.Link>
         </Nav>
       </Navbar>
       <Container id="login">
