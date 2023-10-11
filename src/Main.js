d688b9801b376b0bc60eea72d0156405944bf8a8
ganyunhee <eunicenellgan@gmail.com>
2023-10-11 15:54:00 +0900

    Fix main page background and layout issues, Add cropped background image

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,8 @@ const Main = () => {
                 <h1 id="title">AI로 보다 똑똑한 상담을!</h1>
                 <p id="subtitle">지금 바로 상담 내용을 자동으로 분석하고 정리해주는<br/>똑똑한 AI와 함께, 편리한 콜상담을 경험해보세요.</p>
             </Container>
-            <img id="main_bg"src="/bold/main_bg.png"></img>
+            <div id="main_bg"></div>
+            
         </>
     );
 }
