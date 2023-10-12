23518f665140120c12daddd3e0868a916616192c
ganyunhee <eunicenellgan@gmail.com>
2023-10-12 21:47:14 +0900

    Add alt message to logo2 on main page

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
     return (
         <>
             <Container id="main">
-                <img class="logo" src="/bold/logo2.png"></img>
+                <img class="logo" src="/bold/logo2.png" alt=""></img>
                 <h1 id="title">AI로 보다 똑똑한 상담을!</h1>
                 <p id="subtitle">지금 바로 상담 내용을 자동으로 분석하고 정리해주는<br/>똑똑한 AI와 함께, 편리한 콜상담을 경험해보세요.</p>
                 <Container id="main_button">
