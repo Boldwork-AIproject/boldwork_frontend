edd81b75d4f2839979bb9706cce1ba832fb2db74
ganyunhee <eunicenellgan@gmail.com>
2023-10-13 00:10:30 +0900

    Add Large, Medium, Small Button components

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,8 @@ import './App.css';
 import { ButtonLarge } from './styledComponents.js'
 import { Container } from 'react-bootstrap';
 
+import { Link } from 'react-router-dom';
+
 const Main = () => {
     return (
         <>
@@ -12,6 +14,7 @@ const Main = () => {
                 <p id="subtitle">지금 바로 상담 내용을 자동으로 분석하고 정리해주는<br/>똑똑한 AI와 함께, 편리한 콜상담을 경험해보세요.</p>
                 <Container id="main_button">
                     <ButtonLarge>상담 업로드하기</ButtonLarge>
+                    <Link to="/test_router" className="ms-5">Components 확인</Link>
                 </Container>
             </Container>
             <div id="main_bg"></div>
