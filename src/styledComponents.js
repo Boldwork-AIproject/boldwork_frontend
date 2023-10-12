b8f0605592115d5173ee0d07f7e5bec826fe667d
ganyunhee <eunicenellgan@gmail.com>
2023-10-13 00:33:28 +0900

    Edit button font size

--- src/styledComponents.js
+++ src/styledComponents.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+/* Buttons */
 export const ButtonLarge = styled.button`
     background-color: var(--primary-100);
     color: white;
@@ -10,27 +11,31 @@ export const ButtonLarge = styled.button`
     width: 190px;
     border-radius: 8px;
     text-align: center;
+    margin: 10px;
 `
 export const ButtonMedium = styled.button`
     background-color: var(--primary-100);
     color: white;
     border: none;
     font-size: var(--body-3);
-    font-weight: bold;
-    height: 42px;
-    width: 160px;
+    font-weight: medium;
+    height: 40px;
+    width: 120px;
     border-radius: 8px;
     text-align: center;
+    margin: 10px;
 `
 export const ButtonSmall = styled.button`
     background-color: var(--primary-100);
     color: white;
     border: none;
     font-size: var(--body-5);
-    font-weight: bold;
+    font-weight: regular;
     height: 38px;
     width: 72px;
     border-radius: 8px;
     text-align: center;
+    margin: 10px;
 `
+
 
