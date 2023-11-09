9c62a9abacca1820ba3831e5f0df8c096c13f011
ganyunhee <eunicenellgan@gmail.com>
2023-11-09 12:16:19 +0900

    Add working CustomerInfo

--- src/pages/CustomerInfo.jsx
+++ src/pages/CustomerInfo.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "../axios.js";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Container } from "../styledComponents";
@@ -10,13 +11,14 @@ import FilterModal from "../components/FilterModal";
 
 export default function CustomerInfo() {
   const navigate = useNavigate();
-  const pages = [1, 2, 3, 4];
-  const [selectedPage, setSelectedPage] = useState(1);
+  const [pages, setPages] = useState([1, 2, 3, 4]);
+  const [totalPage, setTotalPage] = useState(4);
+  const [selectedPage, setSelectedPage] = useState("1");
   const [showModal, setShowModal] = useState(false);
   const [data, setData] = useState([
     {
       id: 0,
-      name: "김상호",
+      name: "권준수",
       phone: "010-3948-3106",
     },
     {
@@ -35,9 +37,39 @@ export default function CustomerInfo() {
       phone: "010-2904-4985",
     },
   ]);
+  
   const handleDelete = (idToDelete) => {
     setData(data.filter((item) => item.id !== idToDelete));   
   };
+
+  const getCustomerList = async (page) => {
+    try {
+      const response = await axios.get(`http://localhost:8000/info/?page=${page}`, {
+        withCredentials: true,
+      });
+
+      console.log(response.data);
+      const { message, totalPage, data: responseData } = response.data;
+      const transformedData = responseData.map(({ customer_id, customer_name, customer_phone }) => ({
+        id: customer_id,
+        name: customer_name,
+        phone: customer_phone,
+      }));
+
+      setData(transformedData);
+      setTotalPage(totalPage);
+      setPages([...Array(totalPage).keys()].map((el) => el + 1));
+
+    } catch (error) {
+      console.error('Error fetching customer list:', error);
+    }
+  };
+
+  useEffect(() => {
+    getCustomerList(selectedPage);
+  }, [selectedPage]);
+  
+
   return (
     <Container>
       {showModal ? (
@@ -84,6 +116,7 @@ export default function CustomerInfo() {
               <PageUnselected
                 onClick={() => {
                   setSelectedPage(el);
+                  //getCustomerList(selectedPage);
                 }}
               >
                 {el}
